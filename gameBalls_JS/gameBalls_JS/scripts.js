93cf657292624ff27c63eb033af56d42aa49cfd9
LipoHovhannisyan2002 <51203233+LipoHovhannisyan2002@users.noreply.github.com>
2023-12-17 23:38:40 +0400

    Add files via upload

---
+++ gameBalls_JS/gameBalls_JS/scripts.js
@@ -0,0 +1,178 @@
+const canvas = document.getElementById('screen');
+const ctx = canvas.getContext('2d'); // Ctx = context
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const platformHeight = 20;
+const maxCircles = 15;
+const circles = [];
+const gravity = 1000; // Earth's gravity 
+const dampening = 0.5;
+const circlePool = [];
+
+
+function Circle(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocityY = 15; // standart velocity by Y 
+    this.velocityX = 0;
+
+    this.draw = function () {
+        ctx.beginPath();
+        const gradient = ctx.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.radius);
+        gradient.addColorStop(0, this.color);
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false); // used Math.PI = 3.14 = Full Radius of circle is 2*PI
+        ctx.fillStyle = gradient;
+        ctx.shadowColor = '#acacac';
+        ctx.shadowBlur = 10;
+        ctx.fill();
+        ctx.closePath();
+    };
+
+    this.update = function (deltaTime) {
+        this.velocityY += gravity * deltaTime;
+        this.y += this.velocityY * deltaTime;
+
+        this.x += this.velocityX * deltaTime;
+
+        
+        if (this.x - this.radius < 0) {
+            this.x = this.radius;
+            this.velocityX *= -dampening;
+        } else if (this.x + this.radius > canvas.width) {
+            this.x = canvas.width - this.radius;
+            this.velocityX *= -dampening;
+        }
+
+        if (this.y + this.radius > canvas.height - platformHeight) {
+            this.y = canvas.height - platformHeight - this.radius;
+            this.velocityY *= -dampening;
+        }
+
+        // Collision
+        circles.forEach(otherCircle => {
+            if (otherCircle !== this) {
+                const dx = this.x - otherCircle.x;
+                const dy = this.y - otherCircle.y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < this.radius + otherCircle.radius) {
+                    const angle = Math.atan2(dy, dx);
+                    const overlap = this.radius + otherCircle.radius - distance;
+
+                    
+                    this.x += (overlap / 2) + Math.cos(angle);
+                    this.y += (overlap / 2) + Math.sin(angle);
+
+                    otherCircle.x -= (overlap / 2) * Math.cos(angle);
+                    otherCircle.y -= (overlap / 2) * Math.sin(angle);
+
+                    // Reflect velocities
+                    const normalX = dx / distance;
+                    const normalY = dy / distance;
+                    const relativeVelocityX = this.velocityX - otherCircle.velocityX;
+                    const relativeVelocityY = this.velocityY - otherCircle.velocityY;
+                    const relativeVelocity = relativeVelocityX * normalX +relativeVelocityY * normalY;
+
+                    this.velocityX -= 1 * relativeVelocity * normalX;
+                    this.velocityY -= 1 * relativeVelocity * normalY;
+
+                    otherCircle.velocityX += 1 * relativeVelocity * normalX;
+                    otherCircle.velocityY += 1 * relativeVelocity * normalY;
+                }
+            }
+        });
+
+        this.draw();
+    };
+} 
+
+function spawnCircle(x, y)  {
+    let circle;
+    if (circlePool.length > 1) {
+        circle = circle.pop();
+        circle.x = x;
+        circle.y = y;
+        circle.opacity = 1;
+    } else {
+        const radius = 40;
+        const color = `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
+        if(getcircle()){
+            
+            circle = new Circle(x, y, radius, color);
+        }
+        else{
+            circlePool.pop
+            circlePool.splice(0,circle = new Circle(x, y, radius, color));
+        }
+       
+    }
+        circle.velocityX = (Math.random() - 0.5) * 50;
+
+        circles.push(circle);
+        updateInfo();
+    }
+
+
+const info = document.getElementById('info');
+
+function updateInfo() {
+    
+    info.innerHTML = `Circles on screen: ${circles.length} / ${maxCircles}`;
+}
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    const now = performance.now();
+    const deltaTime = (now - then) / 1000;
+    then = now;
+
+    circles.forEach(circle => {
+        circle.update(deltaTime);
+        circle.draw();
+    });
+
+
+    if (circles.length > maxCircles) {
+        spliceed = circles.splice(0, circles.length - maxCircles);
+        updateInfo();
+        circlePool.push(circle)
+    }
+}
+
+function getcircle(circlePool){
+    if(circlePool==null){
+
+        return null; 
+    }
+    else if (circlePool.length === 0) {
+        updateInfo();
+        console.log("The array is empty");
+       return true
+    }
+    
+    else{
+            updateInfo();
+            console.log("The array is not empty");
+            return false; 
+    }
+
+  }
+     
+    
+
+
+let then = performance.now();
+canvas.addEventListener('click', (e) => {
+    const clickX = e.clientX;
+    const clickY = e.clientY;
+    spawnCircle(clickX, clickY);
+});
+
+animate();
